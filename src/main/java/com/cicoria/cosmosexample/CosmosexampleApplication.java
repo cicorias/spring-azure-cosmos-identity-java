f1c9b130d9a226db301853ea64f5f478f7e0deeb
Shawn Cicoria <github@cicoria.com>
2023-05-03 12:11:31 -0400

    initial

---
+++ src/main/java/com/cicoria/cosmosexample/CosmosexampleApplication.java
@@ -0,0 +1,13 @@
+package com.cicoria.cosmosexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosexampleApplication.class, args);
+	}
+
+}
